f63392ee038f7f39b3b9cfe49dbae1b38f4e1ae7
unknown <aziz1800016090@webmail.uad.ac.id>
2020-06-14 10:05:12 +0700

    biodata

---
+++ mhs.php
@@ -0,0 +1,36 @@
+<html>
+<body>
+<table border="1" align="center">
+<tr>
+     <td width="117">NIM</td>
+     <td width="14">=</td>
+     <td width="218"> <?php echo $_POST["NIM"] ?></td>
+</tr>
+<tr>
+     <td>NAMA</td>
+     <td>=</td>
+     <td><?php echo $_POST["NAMA"]?></td>
+</tr>
+<tr>
+     <td>JENIS KELAMIN</td>
+     <td>=</td>
+     <td><?php echo $_POST["KELAMIN"]?></td>
+</tr>
+<tr>
+     <td>ALAMAT</td>
+     <td>=</td>
+     <td><?php echo $_POST["ALAMAT"]?></td>
+</tr>
+<tr>
+     <td>PROGRAM STUDI</td>
+     <td>=</td>
+     <td><?php echo $_POST["PRODI"]?></td>
+</tr>
+<tr>
+     <td>BAHASA PEMROGRAMAN YANG DIKUASAI</td>
+     <td>=</td>
+     <td><?php echo $_POST["PEMROGRAMAN"]?></td>     
+</tr>
+</table>
+</body>
+</html>
---
+++ outputbiodata.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>Output</title>
+</head>
+<body>
+<table border="1" align="center">
+<tr>
+     <td width="117">NIM</td>
+     <td width="14">=</td>
+     <td width="218"> <?php echo $_POST["NIM"] ?></td>
+</tr>
+<tr>
+     <td>NAMA</td>
+     <td>=</td>
+     <td><?php echo $_POST["NAMA"]?></td>
+</tr>
+<tr>
+     <td>Jenis Kelamin</td>
+     <td>=</td>
+     <td><?php echo $_POST["KELAMIN"]?></td>
+</tr>
+<tr>
+     <td>Program Studi</td>
+     <td>=</td>
+     <td><?php echo $_POST["PRODI"]?></td>
+</tr>
+</table>
+</body>
+</html>
